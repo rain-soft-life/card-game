281a0bfe304a747ac20c57731428685c12cc78c0
rain-soft-life <2390704745@qq.com>
2026-05-07 18:53:01 +0800

    重构public文件

--- baseheadfiles/basehead.h
+++ baseheadfiles/basehead.h
@@ -1,7 +1,15 @@
-#pragma once
-#include<stdio.h>//标准头文件
-#include<stdbool.h>//bool型
-#include<stdint.h>//整型数据
-#include<stdlib.h>//内存释放申请函数
-#include<stddef.h>//size_t类型
-#include<string.h>//字节内存操作
+#ifndef BASEHEAD_H
+#define BASEHEAD_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <termios.h>
+#endif
+
+#endif
